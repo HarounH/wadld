8c130a8d8bb0e64d9e8f1f14c290c75fd488fc2e
Haroun Habeebe <haroun7@gmail.com>
2019-01-31 19:00:38 -0600

    Refactoring step 1: Deprecation

--- deprecated/utils/data_utils.py
+++ deprecated/utils/data_utils.py
@@ -1,5 +1,21 @@
 from collections import defaultdict
 import numpy as np
+import copy
+
+
+def duplicate_linedefs(linedefs):
+    new_linedefs = []
+    for linedef in linedefs:
+        new_linedefs.append(copy.deepcopy(linedef))
+        new_linedefs[-1].back_sidedef = -1
+        if linedef.back_sidedef != -1:
+            nld = (copy.deepcopy(linedef))
+            nld.vertex_start_idx, nld.vertex_end_idx = nld.vertex_end_idx, nld.vertex_start_idx
+            nld.front_sidedef = nld.back_sidedef
+            nld.back_sidedef = -1
+            new_linedefs.append(nld)
+    return new_linedefs
+
 
 def translate_vertex(vertex, offset_x, offset_y):
     ''' Transforms coordinates from wad coordinate space
--- utils/utils.py
+++ utils/utils.py
@@ -20,17 +20,3 @@ class Metadata:
     """
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
-
-
-def duplicate_linedefs(linedefs):
-    new_linedefs = []
-    for linedef in linedefs:
-        new_linedefs.append(copy.deepcopy(linedef))
-        new_linedefs[-1].back_sidedef = -1
-        if linedef.back_sidedef != -1:
-            nld = (copy.deepcopy(linedef))
-            nld.vertex_start_idx, nld.vertex_end_idx = nld.vertex_end_idx, nld.vertex_start_idx
-            nld.front_sidedef = nld.back_sidedef
-            nld.back_sidedef = -1
-            new_linedefs.append(nld)
-    return new_linedefs
